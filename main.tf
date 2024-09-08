6b08f5216e8af55c24a7cbddd6b1aa055f2f3768
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-08 11:31:03 -0500

    Adding .tf files

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+required_version = ">= 1.0.0"
+required_providers {
+aws = {
+source = "hashicorp/aws"
+}
+http = {
+version = "2.1.0"
+}
+random = {
+source = "hashicorp/random"
+version = "3.1.0"
+}
+local = {
+source = "hashicorp/local"
+version = "2.1.0"
+}
+tls = {
+source = "hashicorp/tls"
+version = "3.1.0"
+}
+azurerm = {
+source = "hashicorp/azurerm"
+version = "2.84.0"
+}
+}
+}
